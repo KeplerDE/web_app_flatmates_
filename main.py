a6ab4e72be08042686cd0ace2ff3c9db3ee3c87b
keple <keplerde@gmail.com>
2023-03-29 22:00:23 +0200

    Implementing the bill home form, web form, form submit button

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask.views import MethodView
-from wtforms import Form
-from flask import Flask, render_template
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 """Cоздает новый экземпляр класса Flask, который является основным компонентом приложения Flask.
      Аргумент __name__— это специальная переменная Python, для которой установлено имя текущего модуля,
       что позволяет Flask узнать, где найти ресурсы приложения."""
+
 
 class HomePage(MethodView):
 
@@ -16,18 +17,32 @@ class HomePage(MethodView):
 class BillFormPage(MethodView):
 
     def get(self):
-        return "I am the bill form page!"
-
+        bill_form = BillForm()
+        return render_template('bill_form_page.html', billform=bill_form)
 
 
 class ResultsPage(MethodView):
-    pass
+    def post(self):
+        billform = BillForm(request.form)
+        amount = billform.amount.data
+        return amount
 
 
 class BillForm(Form):
-    pass
+    amount = StringField("Bill Amount: ")
+    period = StringField("Bill Period: ")
+
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in the house: ")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in the house: ")
+
+    button = SubmitField("Calculate")
 
 
-app.add_url_rule('/', view_func=HomePage.as_view('home.page'))
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
-app.run()
+app.add_url_rule('/results',
+                 view_func=ResultsPage.as_view('results_page'))
+app.run(debug=True)
